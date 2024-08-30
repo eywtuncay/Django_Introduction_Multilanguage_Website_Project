8074a3942074e9b39edb2846a25668c438f29c42
eywtuncay <tuncay.albayrak.65@outlook.com>
2024-08-30 09:32:04 +0300

    Add Base Templates

--- solutions/admin.py
+++ solutions/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Services, Solutions
 
-admin.site.register(Solutions)
+class SolutionsAdmin(admin.ModelAdmin):
+    list_display = ("solutions_name", "homepage")
+    list_editable = ("homepage",)
+
+
+admin.site.register(Solutions,SolutionsAdmin)
 admin.site.register(Services)
